6dce1da465b189d703bec16944cb79a1cb3fd9dc
edwin garcia <edwingarbol95@hotmail.com>
2019-06-04 17:55:05 -0500

    v1-2

--- BD/modulo.sql
+++ BD/modulo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-06-2019 a las 17:26:43
+-- Tiempo de generación: 05-06-2019 a las 00:54:31
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 7.2.12
 
@@ -25,10 +25,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `historia clinica`
+-- Estructura de tabla para la tabla `historia_clinica`
 --
 
-CREATE TABLE `historia clinica` (
+CREATE TABLE `historia_clinica` (
   `Id` int(11) NOT NULL,
   `Tratamiento` int(11) NOT NULL,
   `Descripcion` varchar(150) NOT NULL
@@ -73,14 +73,39 @@ CREATE TABLE `tratamiento` (
   `Medico` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `firstname` varchar(30) COLLATE utf8_bin NOT NULL,
+  `lastname` varchar(30) COLLATE utf8_bin NOT NULL,
+  `identification` int(20) NOT NULL,
+  `sexo` varchar(1) COLLATE utf8_bin NOT NULL,
+  `email` varchar(50) COLLATE utf8_bin NOT NULL,
+  `password` varchar(50) COLLATE utf8_bin NOT NULL,
+  `photo` varchar(50) COLLATE utf8_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `firstname`, `lastname`, `identification`, `sexo`, `email`, `password`, `photo`) VALUES
+(29, 'edwin', 'garcia', 123, '', 'edwin@gmail.com', '$2y$10$47BcgTXq.oR2ZG.kuzSZQODUfzZTfIqUCiQFJGDCY.x', 'photos/0b6657d2e463024125b4320c45724682.jpg'),
+(30, 'alejo', 'garcia', 6546, '', 'edwin95166@ggmail.com', '$2y$10$0O59/R486YSf6oh4O36OvOiwH/66gyruhj1hllxz6Xi', 'photos/1ee31541-ea0d-40aa-8738-e0f8ee77862b.jpg');
+
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `historia clinica`
+-- Indices de la tabla `historia_clinica`
 --
-ALTER TABLE `historia clinica`
+ALTER TABLE `historia_clinica`
   ADD PRIMARY KEY (`Id`);
 
 --
@@ -100,6 +125,22 @@ ALTER TABLE `paciente`
 --
 ALTER TABLE `tratamiento`
   ADD PRIMARY KEY (`Codigo`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=226;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
